71f1c0c58fd302940c26810c9eb223fffc12be27
Amirhossein_Soleimani <ah.soleimani2@aut.ac.ir>
2022-11-06 22:40:21 +0330

    Creating Application Class

---
+++ android/app/src/main/kotlin/com/example/complete_advanced_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.complete_advanced_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/presentation/color_manager.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+class ColorManager{
+  // static Color primary = '#ED9728';
+  // static Color primary = '#ED9728';
+  // static Color primary = '#ED9728';
+  // static Color primary = '#ED9728';
+}
