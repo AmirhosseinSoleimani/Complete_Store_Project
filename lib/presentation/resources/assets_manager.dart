ee3c99b25690bf4e13f4e5f6d10856fcf40b99f4
Amirhossein_Soleimani <ah.soleimani2@aut.ac.ir>
2022-11-08 00:31:38 +0330

    Add Assets for OnBoarding Screen

--- lib/presentation/resources/assets_manager.dart
+++ lib/presentation/resources/assets_manager.dart
@@ -2,4 +2,8 @@
 const String imagePath = 'lib/assets/images';
 class ImageAssets{
   static const String splashLogo = '$imagePath/splash_logo.png';
+  static const String onBoardingLogo1 = '$imagePath/onboarding_logo1.svg';
+  static const String onBoardingLogo2 = '$imagePath/onboarding_logo2.svg';
+  static const String onBoardingLogo3 = '$imagePath/onboarding_logo3.svg';
+  static const String onBoardingLogo4 = '$imagePath/onboarding_logo4.svg';
 }
--- lib/presentation/resources/string_manager.dart
+++ lib/presentation/resources/string_manager.dart
@@ -1,4 +1,14 @@
 
 class AppString{
   static const String noRouteFound = 'No Route Found';
+  static const String onBoardingTitle1 = 'See The Best Course #1';
+  static const String onBoardingTitle2 = 'See The Best Course #2';
+  static const String onBoardingTitle3 = 'See The Best Course #3';
+  static const String onBoardingTitle4 = 'See The Best Course #4';
+
+  static const String onBoardingSubTitle1 = 'The app is an awesome flutter application using clean architecture #1';
+  static const String onBoardingSubTitle2 = 'The app is an awesome flutter application using clean architecture #2';
+  static const String onBoardingSubTitle3 = 'The app is an awesome flutter application using clean architecture #3';
+  static const String onBoardingSubTitle4 = 'The app is an awesome flutter application using clean architecture #4';
+
 }
