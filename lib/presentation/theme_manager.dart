30e5df936bd4309eab414ad06627dfbcbfa2a1cc
Amirhossein_Soleimani <ah.soleimani2@aut.ac.ir>
2022-11-07 12:26:23 +0330

    Add Text Form Field Theme

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -57,8 +57,62 @@ ThemeData getApplicationTheme(){
       subtitle1: getMediumStyle(color: ColorManager.lightGrey,fontSize: FontSize.s14),
       caption: getRegularStyle(color: ColorManager.grey1),
       bodyText1: getRegularStyle(color: ColorManager.grey),
-  )
+  ),
 
     // input decoration theme(text form field)
+    inputDecorationTheme: InputDecorationTheme(
+      contentPadding: const EdgeInsets.all(AppPadding.p8),
+
+      // hint style
+      hintStyle: getRegularStyle(
+          color: ColorManager.grey1,
+      ),
+
+      // label style
+      labelStyle: getMediumStyle(
+          color: ColorManager.darkGrey,
+      ),
+
+      // error style
+      errorStyle: getRegularStyle(
+          color: ColorManager.error,
+      ),
+
+      // enabled border
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: ColorManager.grey,
+          width: AppSize.s1_5
+        ),
+        borderRadius: const BorderRadius.all(Radius.circular(AppSize.s8)),
+      ),
+
+      // focused border
+      focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+            color: ColorManager.primary,
+            width: AppSize.s1_5
+        ),
+        borderRadius: const BorderRadius.all(Radius.circular(AppSize.s8)),
+      ),
+
+      // error border
+      errorBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+            color: ColorManager.error,
+            width: AppSize.s1_5
+        ),
+        borderRadius: const BorderRadius.all(Radius.circular(AppSize.s8)),
+      ),
+
+      // focus error border
+      focusedErrorBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+            color: ColorManager.primary,
+            width: AppSize.s1_5
+        ),
+        borderRadius: const BorderRadius.all(Radius.circular(AppSize.s8)),
+      )
+    ),
   );
 }
