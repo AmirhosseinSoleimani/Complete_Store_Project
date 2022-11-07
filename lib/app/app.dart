8fe087a69426fbecbbe5e4d18f76bc33ef7e7046
Amirhossein_Soleimani <ah.soleimani2@aut.ac.ir>
2022-11-07 23:39:59 +0330

    Assets Start Splash Screen

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,3 +1,4 @@
+import 'package:complete_advanced_project/presentation/resources/routes_manager.dart';
 import 'package:complete_advanced_project/presentation/resources/theme_manager.dart';
 import 'package:flutter/material.dart';
 
@@ -16,7 +17,10 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: getApplicationTheme(),
+      onGenerateRoute: RouteGenerator.getRoute,
+      initialRoute: Routes.splashRoute,
     );
   }
 }
---
+++ lib/presentation/resources/assets_manager.dart
@@ -0,0 +1,5 @@
+
+const String imagePath = 'lib/assets/images';
+class ImageAssets{
+  static const String splashLogo = '$imagePath/splash_logo.png';
+}
