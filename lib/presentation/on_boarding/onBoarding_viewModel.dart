3dcf30205ad244cf515389191c4e682111e90aa8
Amirhossein_Soleimani <ah.soleimani2@aut.ac.ir>
2022-11-10 21:20:37 +0330

    Implementation OnBoarding Screen Part 1

---
+++ lib/presentation/on_boarding/onBoarding_viewModel.dart
@@ -0,0 +1,26 @@
+import 'package:complete_advanced_project/presentation/base/base_viewModel.dart';
+
+class OnBoardingViwModel extends BaseViewModel{
+
+  // inputs
+  @override
+  void dispose() {
+    // TODO: implement dispose
+  }
+
+  @override
+  void start() {
+    // TODO: implement start
+  }
+
+}
+
+// inputs mean the orders that our view model will receive from our view
+abstract class OnBoardingViewModelInputs{
+
+}
+
+// outputs mean data or results that will be send from our view model to our view
+abstract class OnBoardingViewModelOutputs{
+
+}
--- lib/presentation/on_boarding/on_boarding.dart
+++ lib/presentation/on_boarding/on_boarding.dart
@@ -1,5 +1,9 @@
+import 'package:complete_advanced_project/presentation/resources/assets_manager.dart';
 import 'package:complete_advanced_project/presentation/resources/color_manager.dart';
+import 'package:complete_advanced_project/presentation/resources/string_manager.dart';
+import 'package:complete_advanced_project/presentation/resources/values_manager.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class OnBoardingView extends StatefulWidget {
   const OnBoardingView({Key? key}) : super(key: key);
@@ -9,11 +13,64 @@ class OnBoardingView extends StatefulWidget {
 }
 
 class _OnBoardingViewState extends State<OnBoardingView> {
+  late final List<SliderObject> _list = _getSliderData();
+  PageController _pageController = PageController(initialPage: 0);
+  int _currentIndex = 0;
+
+  List<SliderObject> _getSliderData() => [
+    SliderObject(
+        AppString.onBoardingSubTitle1,
+        AppString.onBoardingTitle1,
+        ImageAssets.onBoardingLogo1,
+    ),
+    SliderObject(
+      AppString.onBoardingSubTitle2,
+      AppString.onBoardingTitle2,
+      ImageAssets.onBoardingLogo2,
+    ),
+    SliderObject(
+      AppString.onBoardingSubTitle3,
+      AppString.onBoardingTitle3,
+      ImageAssets.onBoardingLogo3,
+    ),
+    SliderObject(
+      AppString.onBoardingSubTitle4,
+      AppString.onBoardingTitle4,
+      ImageAssets.onBoardingLogo4,
+    )
+  ];
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: ColorManager.white,
-      body: const Center(child: Text('Welcome to on boarding')),
+      appBar: AppBar(
+        elevation: AppSize.s1_5,
+        systemOverlayStyle: SystemUiOverlayStyle(
+          statusBarColor: ColorManager.white,
+          statusBarBrightness: Brightness.dark,
+          statusBarIconBrightness: Brightness.dark
+        ),
+      ),
+      body: PageView.builder(
+        controller: _pageController,
+          itemCount: _list.length,
+          onPageChanged: (index){
+          setState(() {
+            _currentIndex = index;
+          });
+          },
+          itemBuilder: (context,index){
+            // return OnBoardingPage
+          })
     );
   }
+}
+
+class SliderObject{
+  String title;
+  String subTitle;
+  String image;
+  SliderObject(this.title,this.subTitle,this.image);
 }
