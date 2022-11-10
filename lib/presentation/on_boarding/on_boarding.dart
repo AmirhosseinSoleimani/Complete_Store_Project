4415177966fbeefb0a73d88180069491ea596c82
Amirhossein_Soleimani <ah.soleimani2@aut.ac.ir>
2022-11-10 21:38:02 +0330

    Implementation OnBoarding Screen Part 2

--- lib/presentation/on_boarding/on_boarding.dart
+++ lib/presentation/on_boarding/on_boarding.dart
@@ -68,6 +68,41 @@ class _OnBoardingViewState extends State<OnBoardingView> {
   }
 }
 
+class OnBoardingPage extends StatelessWidget{
+  SliderObject _sliderObject;
+  OnBoardingPage(this._sliderObject,{Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context){
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        const SizedBox(
+          height: AppSize.s40,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(AppPadding.p8),
+          child: Text(
+            _sliderObject.title,
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.headline1,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(AppPadding.p8),
+          child: Text(
+            _sliderObject.subTitle,
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.subtitle1,
+          ),
+        ),
+        const SizedBox(
+          height: AppSize.s60,
+        )
+      ],
+    );
+  }
+}
+
 class SliderObject{
   String title;
   String subTitle;
