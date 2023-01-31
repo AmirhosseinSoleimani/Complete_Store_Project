65edaab14da8baf21f126e845f37a33780d109a5
Amirhossein_Soleimani <ah.soleimani2@aut.ac.ir>
2023-02-01 00:40:58 +0330

    Adding Extension on Data Sources in Data Layer

--- lib/data/network/error_handler.dart
+++ lib/data/network/error_handler.dart
@@ -1,3 +1,5 @@
+import 'package:complete_advanced_project/data/network/failure.dart';
+
 enum DataSource{
   success,
   noContent,
@@ -12,6 +14,37 @@ enum DataSource{
   sendTimeout,
   cacheError,
   noInternetConnection,
+}
+
+extension DataSourceExtension on DataSource{
+  Failure getFailure(){
+    switch(this) {
+      case DataSource.badRequest:
+        return Failure(ResponseCode.badRequest, ResponseMessage.badRequest);
+      case DataSource.forbidden:
+        return Failure(ResponseCode.forbidden, ResponseMessage.forbidden);
+      case DataSource.unAuthorised:
+        return Failure(ResponseCode.unAuthorised, ResponseMessage.unAuthorised);
+      case DataSource.notFound:
+        return Failure(ResponseCode.notFound, ResponseMessage.notFound);
+      case DataSource.internalServerError:
+        return Failure(ResponseCode.internalServerError, ResponseMessage.internalServerError);
+      case DataSource.connectTimeout:
+        return Failure(ResponseCode.connectTimeout, ResponseMessage.connectTimeout);
+      case DataSource.cancel:
+        return Failure(ResponseCode.cancel, ResponseMessage.cancel);
+      case DataSource.receiveTimeout:
+        return Failure(ResponseCode.receiveTimeout, ResponseMessage.receiveTimeout);
+      case DataSource.sendTimeout:
+        return Failure(ResponseCode.sendTimeout, ResponseMessage.sendTimeout);
+      case DataSource.cacheError:
+        return Failure(ResponseCode.cacheError, ResponseMessage.cacheError);
+      case DataSource.noInternetConnection:
+        return Failure(ResponseCode.noInternetConnection, ResponseMessage.noInternetConnection);
+      default:
+        return Failure(ResponseCode.unknown, ResponseMessage.unknown);
+    }
+  }
 }
 
 class ResponseCode{
@@ -33,4 +66,25 @@ class ResponseCode{
   static const int sendTimeout = -5;
   static const int cacheError = -6;
   static const int noInternetConnection = -7;
+}
+
+class ResponseMessage{
+  // API Message Codes
+  static const String success = 'Success'; // success with data
+  static const String noContent = 'Success with no Content'; // success with no content
+  static const String badRequest = 'Bad request, try again later'; // failure, Api rejected the request
+  static const String forbidden = 'Forbidden request, try again later'; // failure, Api rejected the request
+  static const String unAuthorised = 'User is unauthorised, try again later'; // failure user is not authorised
+  static const String notFound = 'Url is not found, try again later'; // failure, Api url is not correct and not found
+  static const String internalServerError = 'Some thing went wrong, try again later'; // failure, crash happened is server side
+
+
+  // Local Message Code
+  static const String unknown = 'Some thing went wrong, try again later';
+  static const String connectTimeout = 'Time out error, try again later';
+  static const String cancel = 'Request was cancelled, try again later';
+  static const String receiveTimeout = 'Time out error, try again later';
+  static const String sendTimeout = 'Time out error, try again later';
+  static const String cacheError = 'Cache error, try again later';
+  static const String noInternetConnection = 'Please check your internet connection';
 }
